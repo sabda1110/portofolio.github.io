7f451c04dd054d19427e6b8392fbd88d72eee2fa
Sabda1110 <sabdasetiawan206@gmail.com>
2023-08-08 15:00:36 +0700

    Membuat target link

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,10 +1,20 @@
 const Social = () => {
   return (
     <div className="home__social">
-      <a href="" className="home__social-icon" target="_blank">
+      <a
+        href="https://www.instagram.com/sabda1110"
+        className="home__social-icon"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i className="uil uil-instagram"></i>
       </a>
-      <a href="" className="home__social-icon" target="_blank">
+      <a
+        href="https://github.com/sabda1110"
+        className="home__social-icon"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i className="uil uil-github-alt"></i>
       </a>
       <a href="" className="home__social-icon" target="_blank">
